f0005333496ab0535a6bd0f2af07d8c9137d7c5a
Anselmo Oliveira <anselmo.oliveira@conexasaude.com.br>
2026-04-13 23:18:58 -0300

    fix

--- dags/k8.py
+++ dags/k8.py
@@ -160,7 +160,6 @@ execution_config = ExecutionConfig(
 render_config = RenderConfig(
     load_method=LoadMode.DBT_LS,
     dbt_project_path=SCHEDULER_DBT_PATH,
-    dbt_executable_path=Path("/home/airflow/.local/bin/dbt"),
     # select=["tag:daily"],   # filtra por tag/pasta se necessário
     # exclude=["tag:skip"],
     emit_datasets=False,

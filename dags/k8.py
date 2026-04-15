cc3dd4e8a5b050178bcf93f2d74190be713a9895
Anselmo Oliveira <anselmo.oliveira@conexasaude.com.br>
2026-04-15 10:45:16 -0300

    fix

--- dags/k8.py
+++ dags/k8.py
@@ -19,7 +19,7 @@ from pathlib import Path
 
 from cosmos import DbtDag, ExecutionConfig, ProjectConfig, RenderConfig
 from cosmos.config import ProfileConfig
-from cosmos.constants import ExecutionMode, InvocationMode, LoadMode
+from cosmos.constants import ExecutionMode, LoadMode
 
 from cosmos.profiles import TrinoLDAPProfileMapping
 from kubernetes.client import models as k8s
@@ -149,7 +149,6 @@ project_config = ProjectConfig(
 
 execution_config = ExecutionConfig(
     execution_mode=ExecutionMode.KUBERNETES,
-    invocation_mode=InvocationMode.SUBPROCESS,
     dbt_project_path=POD_DBT_PATH,
     dbt_executable_path="/usr/local/bin/dbt",          # caminho no pod (dbt-trino:latest)
 )

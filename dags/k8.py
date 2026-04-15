3566aec976d99255d73791461ff80332b9cd525e
Anselmo Oliveira <anselmo.oliveira@conexasaude.com.br>
2026-04-15 10:25:29 -0300

    revert

--- dags/k8.py
+++ dags/k8.py
@@ -19,7 +19,7 @@ from pathlib import Path
 
 from cosmos import DbtDag, ExecutionConfig, ProjectConfig, RenderConfig
 from cosmos.config import ProfileConfig
-from cosmos.constants import ExecutionMode, LoadMode
+from cosmos.constants import ExecutionMode, InvocationMode, LoadMode
 
 from cosmos.profiles import TrinoLDAPProfileMapping
 from kubernetes.client import models as k8s
@@ -149,6 +149,7 @@ project_config = ProjectConfig(
 
 execution_config = ExecutionConfig(
     execution_mode=ExecutionMode.KUBERNETES,
+    invocation_mode=InvocationMode.SUBPROCESS,
     dbt_project_path=POD_DBT_PATH,
     dbt_executable_path="/usr/local/bin/dbt",          # caminho no pod (dbt-trino:latest)
 )
@@ -160,7 +161,7 @@ execution_config = ExecutionConfig(
 render_config = RenderConfig(
     load_method=LoadMode.DBT_LS,
     dbt_project_path=SCHEDULER_DBT_PATH,
-    # dbt_executable_path não necessário: ASTRONOMER_COSMOS_DBT_VIRTUALENV_PATH no Dockerfile
+    dbt_executable_path="/opt/airflow/dbt_venv/bin/dbt",  # venv no dag-processor
     # select=["tag:daily"],   # filtra por tag/pasta se necessário
     # exclude=["tag:skip"],
     emit_datasets=False,
